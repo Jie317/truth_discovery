935eb0b2360fda0969dc9fac2833752678147e30
Pierre <pierre.coquerel@mines-ales.org>
2017-02-08 21:37:48 +0100

    Test avec BornIn + Picasso

--- src/main/java/ema/mission/TruthDiscovery/App.java
+++ src/main/java/ema/mission/TruthDiscovery/App.java
@@ -1,13 +1,29 @@
 package ema.mission.TruthDiscovery;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	scrape("Picasso", "BornIn");
     }
+    
+    public static void scrape(String sujet, String predicat){
+	    Document doc;
+	    try{
+	        doc =Jsoup.connect("https://www.google.com/search?as_q="+sujet+"+"+predicat+"&as_eq=&as_nlo=&as_nhi=&lr=lang_en&cr=countryCA&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&tbs=&as_filetype=&as_rights=").userAgent("Mozilla").ignoreHttpErrors(true).timeout(0).get();
+	        Elements divResult=doc.getElementsByClass("");
+	        System.out.println(doc.getElementById("res"));
+	    }
+	    
+	    catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
 }
