e83ddba65f29c35225e2dacb1d03aefdd940cc8c
Charlie <Charlie@Charlie-PC.home>
2017-02-08 21:50:21 +0100

    Titre et lien des premières recherches

--- src/main/java/ema/mission/TruthDiscovery/App.java
+++ src/main/java/ema/mission/TruthDiscovery/App.java
@@ -18,8 +18,12 @@ public class App
 	    Document doc;
 	    try{
 	        doc =Jsoup.connect("https://www.google.com/search?as_q="+sujet+"+"+predicat+"&as_eq=&as_nlo=&as_nhi=&lr=lang_en&cr=countryCA&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&tbs=&as_filetype=&as_rights=").userAgent("Mozilla").ignoreHttpErrors(true).timeout(0).get();
-	        Elements divResult=doc.getElementsByClass("");
-	        System.out.println(doc.getElementById("res"));
+	        Elements divResult=doc.getElementsByClass("g");
+	        for(Element e:divResult){
+	        	String titre=e.getElementsByClass("r").get(0).text();
+	        	String lien=e.getElementsByClass("r").get(0).getElementsByAttribute("href").attr("href").replace("/url?q=", "");
+	        	System.out.println(titre+" "+lien);
+	        }
 	    }
 	    
 	    catch (IOException e) {
