7da33f0651c7b082cee99d31283fa0e29d3fad5c
Pierre <pierre.coquerel@mines-ales.org>
2017-02-16 17:00:46 +0100

    Fin de l'interface de log

--- src/main/java/ema/mission/controller/LogController.java
+++ src/main/java/ema/mission/controller/LogController.java
@@ -8,35 +8,73 @@ import java.awt.event.KeyListener;
 import ema.mission.model.User;
 import ema.mission.view.Log;
 
-public class LogController implements ActionListener,KeyListener {
+public class LogController implements ActionListener, KeyListener {
 
 	private Log view;
 	private User user;
 
 	public LogController(Log log, User user) {
 		this.view = log;
-		this.user=user;
+		this.user = user;
 	}
 
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (e.getSource() == this.view.getEmail()) {
+				this.view.getPass().requestFocusInWindow();
+			} else if (e.getSource() == this.view.getPass()) {
+				this.view.getBtnConnexion().requestFocusInWindow();
+			} else if (e.getSource() == this.view.getBtnConnexion()) {
+				this.user = Bdd.authenticate(this.view.getEmail().getText(),
+						new String(this.view.getPass().getPassword()));
+			}
+		}
 	}
 
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.getSource() == this.view.getBtnConnexion()) {
+			this.user = Bdd.authenticate(this.view.getEmail().getText(), new String(this.view.getPass().getPassword()));
+		}
 	}
 
-	
+	/**
+	 * @return the view
+	 */
+	public Log getView() {
+		return view;
+	}
+
+	/**
+	 * @param view
+	 *            the view to set
+	 */
+	public void setView(Log view) {
+		this.view = view;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user
+	 *            the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 }
--- src/main/java/ema/mission/view/Log.java
+++ src/main/java/ema/mission/view/Log.java
@@ -1,14 +1,16 @@
 package ema.mission.view;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 import ema.mission.controller.LogController;
@@ -19,9 +21,10 @@ public class Log extends JFrame {
 	private User user;
 	private LogController controller;
 
-	private final int LARGEUR_FENETRE = 600, HAUTEUR_FENETRE = 600;
-	private JPanel panel;
+	private final int LARGEUR_FENETRE = 400, HAUTEUR_FENETRE = 200;
 	private JButton btnConnexion;
+	private JTextField email;
+	private JPasswordField pass;
 
 	public Log(User u) {
 		this.user = u;
@@ -44,26 +47,136 @@ public class Log extends JFrame {
 		// fenêtre (arrêt du procéssus)
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		//Ajout d'un panel qui contiendra tout
-		panel = new JPanel();
-		panel.setBounds(0, 0, 594, 571);
-		GridLayout gl = new GridLayout(4, 3);
-		gl.setHgap(5); //Cinq pixels d'espace entre les colonnes (H comme Horizontal)
-		gl.setVgap(5); //Cinq pixels d'espace entre les lignes (V comme Vertical) 
-		panel.setLayout(gl);
-		this.getContentPane().add(panel);
+		this.setLayout(new GridBagLayout());
+		
+		JLabel emailLabel = new JLabel("Email: ");
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = gbc.gridy = 0; // la grille commence en (0, 0)
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		 /* une seule cellule sera disponible pour ce composant. */
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.anchor = GridBagConstraints.BASELINE_LEADING; 
+		/* marges */
+		gbc.insets = new Insets(10, 15, 0, 0);
+		this.add(emailLabel,gbc);
+		
+		email = new JTextField();
+		email.addKeyListener(controller);
+		email.setPreferredSize(new Dimension(200,25));
+		gbc.gridx = 1; /* une position horizontalement à droite de l'étiquette */
+		gbc.gridy = 0; /* sur la même ligne que l'étiquette */
+		gbc.gridwidth = GridBagConstraints.REMAINDER; /* il est le dernier composant de sa ligne. */
+		gbc.gridheight = 1; /* une seule cellule verticalement suffit */
+		/* Le composant peut s'étendre sur tout l'espace qui lui est attribué horizontalement. */
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		/* Alignons ce composant sur la même ligne d'écriture que son étiquette. */
+		gbc.anchor = GridBagConstraints.BASELINE;
+		gbc.insets = new Insets(0, 15, 20, 10);
+		this.add(email,gbc);
+		
+		JLabel passLabel = new JLabel("Password: ");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		 /* une seule cellule sera disponible pour ce composant. */
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.anchor = GridBagConstraints.BASELINE_LEADING; 
+		/* marges */
+		gbc.insets = new Insets(10, 15, 0, 0);
+		this.add(passLabel,gbc);
+		
+		pass = new JPasswordField();
+		pass.addKeyListener(controller);
+		pass.setPreferredSize(new Dimension(200,25));
+		gbc.gridx = 1; /* une position horizontalement à droite de l'étiquette */
+		gbc.gridy = 1; /* sur la même ligne que l'étiquette */
+		gbc.gridwidth = GridBagConstraints.REMAINDER; /* il est le dernier composant de sa ligne. */
+		gbc.gridheight = 1; /* une seule cellule verticalement suffit */
+		/* Le composant peut s'étendre sur tout l'espace qui lui est attribué horizontalement. */
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		/* Alignons ce composant sur la même ligne d'écriture que son étiquette. */
+		gbc.anchor = GridBagConstraints.BASELINE;
+		gbc.insets = new Insets(0, 15, 20, 10);
+		this.add(pass,gbc);
 
 		// Ajout d'un bouton de connexion
 		btnConnexion = new JButton("Connexion");
-		btnConnexion.setBounds(227, 330, 142, 23);
-		btnConnexion.setBackground(Color.RED);
-		btnConnexion.setForeground(Color.WHITE);
 		btnConnexion.addActionListener(controller);
-		panel.add(btnConnexion);
+		btnConnexion.addKeyListener(controller);
+		gbc.gridx = 0; /* une position horizontalement à droite de l'étiquette */
+		gbc.gridy = 2; /* sur la même ligne que l'étiquette */
+		gbc.gridwidth = 3;/* il est le dernier composant de sa ligne. */
+		gbc.gridheight = 1; /* une seule cellule verticalement suffit */
+		/* Le composant peut s'étendre sur tout l'espace qui lui est attribué horizontalement. */
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		/* Alignons ce composant sur la même ligne d'écriture que son étiquette. */
+		gbc.anchor = GridBagConstraints.BASELINE;
+		gbc.insets = new Insets(0, 15, 0, 10);
+		this.add(btnConnexion, gbc);
 
 		// Packing et affichage de la JFrame
 		this.pack();
 		this.setVisible(true);
+	}
+
+	/**
+	 * @return the controller
+	 */
+	public LogController getController() {
+		return controller;
+	}
+
+	/**
+	 * @param controller the controller to set
+	 */
+	public void setController(LogController controller) {
+		this.controller = controller;
+	}
+
+	/**
+	 * @return the btnConnexion
+	 */
+	public JButton getBtnConnexion() {
+		return btnConnexion;
+	}
+
+	/**
+	 * @param btnConnexion the btnConnexion to set
+	 */
+	public void setBtnConnexion(JButton btnConnexion) {
+		this.btnConnexion = btnConnexion;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public JTextField getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(JTextField email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the pass
+	 */
+	public JPasswordField getPass() {
+		return pass;
+	}
+
+	/**
+	 * @param pass the pass to set
+	 */
+	public void setPass(JPasswordField pass) {
+		this.pass = pass;
 	}
 
 }
